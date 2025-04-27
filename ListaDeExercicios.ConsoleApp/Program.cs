f1988a1a8dd484305fe7ad239fd15d20a0e5e7b4
Diego Toledo <Diego_toledo3010@hotmail.com>
2025-04-27 02:18:58 -0300

    Criação da Lista de Exercicios.

--- ListaDeExercicios.ConsoleApp/Program.cs
+++ ListaDeExercicios.ConsoleApp/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bem Vindo a lista de exercicios");
         }
     }
 }
