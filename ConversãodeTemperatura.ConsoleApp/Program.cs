77454099239813d5d805f92836bba89fa7799c76
Diego Toledo <Diego_toledo3010@hotmail.com>
2025-04-27 02:25:25 -0300

    ConversãodeTemperatura.ConsoleApp

---
+++ ConversãodeTemperatura.ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConversãodeTemperatura.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                decimal c = 0;
+
+                decimal resultado = 0;
+
+                Console.Clear();
+                Console.WriteLine("--------------------------------------------------");
+                Console.WriteLine("Olá, aqui iremos converter graus Celsius (°C) para graus Fahrenheit (°F)");
+                Console.WriteLine("--------------------------------------------------");
+
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("Para começar, irei precisar da temperatura em graus Celcius (°C) ");
+                Console.WriteLine("----------------------------------------");
+
+                c = Convert.ToDecimal(Console.ReadLine());
+
+                resultado = (c * 9 / 5) + 32;
+
+                Console.WriteLine($"A temperatura é de {resultado} graus Fahrenheit (°F).");
+
+                Console.Write("Deseja continuar? (S/N): ");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+
+                if (opcaoContinuar != "S")
+                    break;
+
+
+            }
+        }
+    }
+}
