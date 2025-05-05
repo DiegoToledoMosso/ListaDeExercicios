8d5c611a7483a462d686f6000fac0759c65ebafa
Diego Toledo <Diego_toledo3010@hotmail.com>
2025-05-05 20:28:33 -0300

    Area do Terreno

---
+++ AreaDoTerreno.ConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace AreaDoTerreno.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                Console.Clear();
+                Console.WriteLine("--------------------------------------------------");
+                Console.WriteLine("Olá, somos a imobiliária Imóbilis, aqui iremos calcular área do seu terreno");
+                Console.WriteLine("--------------------------------------------------");
+
+                Console.WriteLine("Para começar, irei precisar medidas do seu terreno:");
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("Digite a largura do terreno em metros:");
+
+
+                double terrenoLargura = Double.Parse(Console.ReadLine());
+
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("Digite o comprimento do terreno em metros:");
+
+
+                double terrenoComprimento = Double.Parse(Console.ReadLine());
+
+                Console.WriteLine("----------------------------------------");
+
+                if ( terrenoLargura == terrenoComprimento )
+                {
+                    Console.WriteLine("Não vendemos terrenos que não seja retangular!!");
+
+                }
+                else
+                {
+                    double area = terrenoLargura * terrenoComprimento;
+                    Console.WriteLine($"A área do terreno é: {area}metros quadrados.");
+                }
+                                   
+                                
+                Console.Write("Deseja continuar? (S/N): ");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+
+                if (opcaoContinuar != "S")
+                    break;
+            }
+        }
+    }
+}
---
+++ CalcularMediaHarmonica.ConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace CalcularMediaHarmonica.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                // Solicitar ao utilizador para introduzir as notas
+                Console.WriteLine("Introduza as notas do aluno (separadas por espaço):");
+                string notasString = Console.ReadLine();
+
+                // Converter a string de notas em um array de double
+                string[] notasArray = notasString.Split(' ');
+                double[] notas = new double[notasArray.Length];
+
+
+                for (int i = 0; i < notasArray.Length; i++)
+                {
+                    if (double.TryParse(notasArray[i], out notas[i]))
+                    {
+                        // Nota válida
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Erro: A nota {notasArray[i]} é inválida.");
+                        return; // Terminar o programa se houver um erro na entrada
+                    }
+                }
+
+                // Calcular a média harmónica
+                double somaInversa = 0;
+                foreach (double nota in notas)
+                {
+                    somaInversa += 1 / nota;
+                }
+
+                double mediaHarmonica = notas.Length / somaInversa;
+
+                // Mostrar a média harmónica
+                Console.WriteLine($"A média harmónica das notas é: {mediaHarmonica}");
+                Console.Write("Deseja continuar? (S/N): ");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+
+                if (opcaoContinuar != "S")
+                    break;
+            }
+        }
+    }
+}
