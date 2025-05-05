5cdd4e853ef3b4ec0cbf8b61c0664a3636a84e10
Diego Toledo <Diego_toledo3010@hotmail.com>
2025-05-05 19:53:36 -0300

    Volume da caixa

---
+++ CalcularVolumeCaixaRetangular.ConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace CalcularVolumeCaixaRetangular.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+
+                decimal resultado = 0;
+
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("Olá, vamos calcular o volume da sua caixa?");
+                Console.WriteLine("--------------------------------");
+
+                Console.WriteLine("Para começar, irei precisar das medidas de sua caixa:");
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("Digite a medida do comprimento da caixa ");
+
+                decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("Digite a medida do largura da caixa");
+
+                decimal largura = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("Digite a medida do altura da caixa");
+
+                decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("----------------------------------------");
+
+
+
+                resultado = comprimento * largura * altura;
+
+
+                Console.WriteLine("Obrigado pelos dados,o volume da sua caixa é : " + resultado.ToString("F2"));
+                Console.ReadLine();
+                Console.Write("Deseja continuar? (S/N): ");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+
+                if (opcaoContinuar != "S")
+                    break;
+            }
+        }
+    }
+}
